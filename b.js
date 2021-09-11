bd428c6ba396ff174c7287da52d5445df28dc93b
Murcott <67573887+Murcott@users.noreply.github.com>
2021-09-11 02:27:55 -0700

    Create b.js

--- b.js
+++ b.js
@@ -2,19 +2,19 @@ document.write(`
 
      <div class="grid">    
             <div class="item">
-            <a href="/murcott/index.htm"  id="home">Home</a>
+            <a href="/index.htm"  id="home">Home</a>
             </div>
           
             <div class="item">
-            <a href="/murcott/toc.htm" id="toc">Table of Contents</a>
+            <a href="/toc.htm" id="toc">Table of Contents</a>
             </div>
             
             <div class="item">
-            <a href="/murcott/glossary.htm" id="glossary">Glossary</a>
+            <a href="/glossary.htm" id="glossary">Glossary</a>
             </div>
             
             <div class="item">
-            <a href="/murcott/contact.htm" id="contact">Contact</a>
+            <a href="/contact.htm" id="contact">Contact</a>
             </div>
         </div>
 

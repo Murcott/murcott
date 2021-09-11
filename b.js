3bdcc2c69d4d5c9df1b6d47dad46d9144b4b2b5f
Murcott <67573887+Murcott@users.noreply.github.com>
2021-09-11 01:06:48 -0700

    Update b.js

--- b.js
+++ b.js
@@ -1,1 +1,21 @@
+document.write(`
 
+     <div class="grid">    
+            <div class="item">
+            <a href="../index.htm"  id="home">Home</a>
+            </div>
+          
+            <div class="item">
+            <a href="../toc.htm" id="toc">Table of Contents</a>
+            </div>
+            
+            <div class="item">
+            <a href="../glossary.htm" id="glossary">Glossary</a>
+            </div>
+            
+            <div class="item">
+            <a href="../contact.htm" id="contact">Contact</a>
+            </div>
+        </div>
+
+`);
